bc815afa78809efdc145ebd9cd8d720fb07add98
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2025-06-18 18:12:54 -0400

    working on assessment model outputs

--- indicator_processing/automated_download/stock_assessment_output.R
+++ indicator_processing/automated_download/stock_assessment_output.R
@@ -15,7 +15,7 @@ library(ggplot2)
 ####### PULL VARIABLES FOR A SINGLE ASSESSMENT ########
 
 # Need to first access the stock synthesis output files from google drive. Point to drive folder shortcut from google drive desktop.
-setwd("G:/.shortcut-targets-by-id/1ixDqh6nB2x_6OmR4yevLRsssxIZhJlni/Assessment Report.sso files/Spanish Mackerel/SEDAR81")
+setwd("G:/.shortcut-targets-by-id/1ixDqh6nB2x_6OmR4yevLRsssxIZhJlni/Assessment Report.sso files/Scamp Grouper/SEDAR68OA/")
 direct = getwd()
 base=SS_output(dir = direct, printstats = T, covar=T, cormax=0.70, forecast=F)
 base$startyr
@@ -31,13 +31,15 @@ summary(base$recruit)
 # Age-1+ biomass over time
 biomass = base$timeseries[,c("Yr","Era","Bio_smry")]
 biomass = biomass %>% 
-  filter(Era != "FORE")
+  filter(Era != "FORE") %>% 
+  filter(Yr >=2000)
 
 # RECRUITMENT DEVIATIONS
 # log-scale recruitment deviations from the expected mean
 recdev = base$recruit[,c("Yr","era","dev")]
 recdev = recdev %>% 
-  filter(era != "Fore")
+  filter(era != "Fore") %>% 
+  filter(Yr >= 2000)
 
 #LANDINGS/DISCARDS RATIO
 # retained biomass and total catch are listed by fleet, so need to sum across fleets
@@ -47,19 +49,34 @@ ts <- base$timeseries
 
 # Identify all retained biomass columns (Ret_Bio)
 retbio_cols <- grep("^retain\\(B\\):", names(ts), value = TRUE)
+comm_retbio_cols = retbio_cols[1:2]
+rec_retbio_cols = retbio_cols[3:4]
 ts$Ret_Bio <- rowSums(ts[, retbio_cols], na.rm = TRUE)
+ts$Ret_Bio_comm <- rowSums(ts[, comm_retbio_cols], na.rm = TRUE)
+ts$Ret_Bio_rec <- rowSums(ts[, rec_retbio_cols], na.rm = TRUE)
 
 # Identify all dead biomass columns (TotCat)
 totcat_cols <- grep("^dead\\(B\\):", names(ts), value = TRUE)
+comm_totcat_cols = totcat_cols[1:2]
+rec_totcat_cols = totcat_cols[3:4]
 ts$TotCat <- rowSums(ts[, totcat_cols], na.rm = TRUE)
+ts$TotCatComm <- rowSums(ts[, comm_totcat_cols], na.rm = TRUE)
+ts$TotCatRec <- rowSums(ts[, rec_totcat_cols], na.rm = TRUE)
 
-land_disc = ts[,c("Yr","Era","Ret_Bio","TotCat")]
+land_disc = ts[,c("Yr","Era","Ret_Bio","Ret_Bio_comm","Ret_Bio_rec","TotCat","TotCatComm","TotCatRec")]
 
 land_disc$discards = land_disc$TotCat - land_disc$Ret_Bio
 land_disc$ld_ratio = land_disc$Ret_Bio / (land_disc$discards)
 
+land_disc$discards_comm = land_disc$TotCatComm - land_disc$Ret_Bio_comm
+land_disc$ld_ratio_comm = land_disc$Ret_Bio_comm / (land_disc$discards_comm)
+
+land_disc$discards_rec = land_disc$TotCatRec - land_disc$Ret_Bio_rec
+land_disc$ld_ratio_rec = land_disc$Ret_Bio_rec / (land_disc$discards_rec)
+
 land_disc = land_disc %>% 
-  filter(Era != "FORE")
+  filter(Era != "FORE") %>% 
+  filter(Yr >= 2000)
 
 # QUICK PLOTS
 
@@ -68,7 +85,7 @@ ggplot(biomass, aes(x=Yr, y=Bio_smry)) +
   xlab("Year") +
   ylab("Biomass (metric tons)") +
   theme_classic() +
-  ggtitle("Red snapper SEDAR 52")
+  ggtitle("Scamp Grouper SEDAR 68")
 
 
 ggplot(recdev, aes(x=Yr, y=dev)) +
